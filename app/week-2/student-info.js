f944dbb7ab8c9e2b6e9d6dc4210ec57d106ce8f3
pranavbhalla22 <pranav.bhalla@edu.sait.ca>
2025-01-28 03:11:42 -0700

    made changes in this code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Pranav Bhalla</p>
+        <p>Link to Github: 
+        <Link href="https://github.com/pranavbhalla22/cprg306-assignments.git">My Github</Link>    
+        </p>    
+      </main>
+    );
+  }
